8e3ec568ccaffca3c1bfee016ad2cfe9a762f02c
Igar Ramaddhan <igarramaddhan@gmail.com>
2019-02-10 19:45:19 +0700

    finishing first tutorial

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  mode: 'development',
+  output: {
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html")
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+    watchContentBase: true,
+    progress: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+};
